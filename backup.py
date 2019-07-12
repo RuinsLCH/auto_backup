7cd4e5df51e93c065dc6982c832af69d04e9b672
William <william@idisplay.com.tw>
2019-07-12 09:46:04 +0800

    push test

---
+++ backup.py
@@ -0,0 +1,25 @@
+import sys
+from subprocess import Popen, PIPE
+
+
+def execute_command(command, description):
+    print(description),
+    sys.stdout.flush()  # update message
+    execution = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)  # ecxcute command and print ouput
+    output, error = execution.communicate()
+    if len(output.decode('utf-8')) >= 1 :
+        print(output.decode('utf-8'))
+    if execution.returncode:  # return true or false
+        print ('/n') + error.decode("utf-8") #decode translate to utf-8
+        sys.stdout.flush()
+        print('not Done')
+        exit(1)
+    print('Done')
+    sys.stdout.flush()
+
+
+
+execute_command("cd C:/Users/iDisplay/Desktop/auto_backup/auto_backup", "cd to project path")
+execute_command("git add --all", "add all")
+execute_command("git commit -m \"push test\"","test for commit")
+execute_command("git push", "push to github")
